815937bbbe94503d402535e52395e9bad81ebf1c
visrealm <code@visualrealmsoftware.com>
2026-09-21 20:42:55 +0930

    Mount the image given with -b in drive B, not drive A's

--- main.c
+++ main.c
@@ -2044,7 +2044,7 @@ int main(int argc, char **argv)
   if (inita)
    disksys_insert(0, inita);
   if (initb)
-   disksys_insert(1, inita);
+   disksys_insert(1, initb);
 
   /*
    * Get ready to start the emulated Z80.
